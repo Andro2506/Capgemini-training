ff244b8c16e3aa7147f564d40d45cbe5fd28cc8d
UEM <sohamdas0625@gmail.com>
2025-03-20 14:10:44 +0530

    first commit in the training

---
+++ A.java
@@ -0,0 +1,9 @@
+class A{
+	public static void main(String[] args){
+		System.out.print(10);
+		System.out.print('*');
+		System.out.print(5);
+		System.out.print('=');
+		System.out.print(50);
+	}
+}
